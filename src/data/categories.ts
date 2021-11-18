14747864e990b2cbd1cc0cedffd74fbe2023efdb
georghiton-luiz <georghiton@hotmail.com>
2021-11-18 01:11:59 -0300

    Finalizado a listagem de itens

---
+++ src/data/categories.ts
@@ -0,0 +1,19 @@
+import { Category } from '../types/Category'
+
+export const categories: Category = {
+    food: {
+        title: 'Alimentação',
+        color: 'blue',
+        expense: true
+    },
+    rent: {
+        title: 'Aluguel',
+        color: 'brown',
+        expense: true
+    },
+    salary: {
+        title: 'Salário',
+        color: 'vermelho',
+        expense: false
+    }
+}
